eab0fb58fec1a42363a3d057055ea6be7e252200
HWO-admin <168010263+HWO-admin@users.noreply.github.com>
2024-04-24 18:04:07 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.py
@@ -0,0 +1,9 @@
+let kanal = 0
+basic.showNumber(kanal)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+    	
+    } else {
+    	
+    }
+})
